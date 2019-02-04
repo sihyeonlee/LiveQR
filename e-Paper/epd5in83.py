db3a4c0c2e9b0590717b7dae217e77d3db2d1fe2
Sihyeon Lee <sihyeon.dev@gmail.com>
2019-02-04 23:25:49 +0900

    feat : Display on E-Paper[Improved]
    판매처에서 제공하는 E-Paper 데모 스크립트를 수정하였습니다.
    
    기존에는 Clear시 32초, Get_Buffer 7초, Display 21초, Refresh 3초로 총 1분 정도 걸리던 초기 이미지 세팅 시간을 Clear - 16초, Get_Buffer - 1.5초, Display - 17.5초, Refresh 0.5초로 총 36초  정도로 단축하였습니다.

---
+++ e-Paper/epd5in83.py
@@ -0,0 +1,243 @@
+# //*****************************************************************************
+# * | File        :	  epd5in83.py
+# * | Author      :   Waveshare team
+# * | Function    :   Electronic paper driver
+# * | Info        :
+# *----------------
+# * |	This version:   V3.0
+# * | Date        :   2018-11-06
+# * | Info        :   python2 demo
+# * 1.Remove:
+#   digital_write(self, pin, value)
+#   digital_read(self, pin)
+#   delay_ms(self, delaytime)
+#   set_lut(self, lut)
+#   self.lut = self.lut_full_update
+# * 2.Change:
+#   display_frame -> TurnOnDisplay
+#   set_memory_area -> SetWindow
+#   set_memory_pointer -> SetCursor
+#   get_frame_buffer -> getbuffer
+#   set_frame_memory -> display
+# * 3.How to use
+#   epd = epd2in7.EPD()
+#   epd.init(epd.lut_full_update)
+#   image = Image.new('1', (epd5in83.EPD_WIDTH, epd5in83.EPD_HEIGHT), 255)
+#   ...
+#   drawing ......
+#   ...
+#   epd.display(getbuffer(image))
+# ******************************************************************************//
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documnetation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and//or sell
+# copies of the Software, and to permit persons to  whom the Software is
+# furished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS OR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+#
+
+
+import epdconfig
+from PIL import Image
+import RPi.GPIO as GPIO
+import time
+
+# Display resolution
+EPD_WIDTH       = 600
+EPD_HEIGHT      = 448
+
+# EPD7IN5 commands
+PANEL_SETTING                               = 0x00
+POWER_SETTING                               = 0x01
+POWER_OFF                                   = 0x02
+POWER_OFF_SEQUENCE_SETTING                  = 0x03
+POWER_ON                                    = 0x04
+POWER_ON_MEASURE                            = 0x05
+BOOSTER_SOFT_START                          = 0x06
+DEEP_SLEEP                                  = 0x07
+DATA_START_TRANSMISSION_1                   = 0x10
+DATA_STOP                                   = 0x11
+DISPLAY_REFRESH                             = 0x12
+IMAGE_PROCESS                               = 0x13
+LUT_FOR_VCOM                                = 0x20
+LUT_BLUE                                    = 0x21
+LUT_WHITE                                   = 0x22
+LUT_GRAY_1                                  = 0x23
+LUT_GRAY_2                                  = 0x24
+LUT_RED_0                                   = 0x25
+LUT_RED_1                                   = 0x26
+LUT_RED_2                                   = 0x27
+LUT_RED_3                                   = 0x28
+LUT_XON                                     = 0x29
+PLL_CONTROL                                 = 0x30
+TEMPERATURE_SENSOR_COMMAND                  = 0x40
+TEMPERATURE_CALIBRATION                     = 0x41
+TEMPERATURE_SENSOR_WRITE                    = 0x42
+TEMPERATURE_SENSOR_READ                     = 0x43
+VCOM_AND_DATA_INTERVAL_SETTING              = 0x50
+LOW_POWER_DETECTION                         = 0x51
+TCON_SETTING                                = 0x60
+TCON_RESOLUTION                             = 0x61
+SPI_FLASH_CONTROL                           = 0x65
+REVISION                                    = 0x70
+GET_STATUS                                  = 0x71
+AUTO_MEASUREMENT_VCOM                       = 0x80
+READ_VCOM_VALUE                             = 0x81
+VCM_DC_SETTING                              = 0x82
+
+class EPD:
+    def __init__(self):
+        self.reset_pin = epdconfig.RST_PIN
+        self.dc_pin = epdconfig.DC_PIN
+        self.busy_pin = epdconfig.BUSY_PIN
+        self.width = EPD_WIDTH
+        self.height = EPD_HEIGHT
+    
+    # Hardware reset
+    def reset(self):
+        epdconfig.digital_write(self.reset_pin, GPIO.HIGH)
+        epdconfig.delay_ms(200) 
+        epdconfig.digital_write(self.reset_pin, GPIO.LOW)         # module reset
+        epdconfig.delay_ms(200)
+        epdconfig.digital_write(self.reset_pin, GPIO.HIGH)
+        epdconfig.delay_ms(200)   
+
+    def send_command(self, command):
+        epdconfig.digital_write(self.dc_pin, GPIO.LOW)
+        epdconfig.spi_writebyte([command])
+
+    def send_data(self, data):
+        epdconfig.digital_write(self.dc_pin, GPIO.HIGH)
+        epdconfig.spi_writebyte([data])
+        
+    def wait_until_idle(self):
+        print("e-Paper busy")
+        while(epdconfig.digital_read(self.busy_pin) == 0):      # 0: idle, 1: busy
+            epdconfig.delay_ms(100)    
+        print("e-Paper busy release")
+        
+    def init(self):
+        if (epdconfig.module_init() != 0):
+            return -1
+        # EPD hardware init start
+        self.reset()
+        
+        self.send_command(POWER_SETTING)
+        self.send_data(0x37)
+        self.send_data(0x00)
+        self.send_command(PANEL_SETTING)
+        self.send_data(0xEF)
+        self.send_data(0x08)
+        self.send_command(BOOSTER_SOFT_START)
+        self.send_data(0xc7)
+        self.send_data(0xcc)
+        self.send_data(0x28)
+        self.send_command(POWER_ON)
+        self.wait_until_idle()
+        self.send_command(PLL_CONTROL)
+        self.send_data(0x19)                                    # 143Hz
+        self.send_command(TEMPERATURE_CALIBRATION)
+        self.send_data(0x00)
+        self.send_command(VCOM_AND_DATA_INTERVAL_SETTING)
+        self.send_data(0x77)
+        self.send_command(TCON_SETTING)
+        self.send_data(0x22)
+        self.send_command(SPI_FLASH_CONTROL)
+        self.send_data(0x00)
+        self.send_command(TCON_RESOLUTION)
+        self.send_data(0x02)     #source 600
+        self.send_data(0x58)
+        self.send_data(0x01)     #gate 448
+        self.send_data(0xC0)
+        self.send_command(VCM_DC_SETTING)
+        self.send_data(0x28)      #decide by LUT file change 1e
+        self.send_command(0xe5)
+        self.send_data(0x03)
+        
+        # EPD hardware init end
+        return 0
+
+    def getbuffer(self, image):
+        start_time = time.time()
+        imwidth, imheight = image.size
+        pixels = image.load()
+        print("imwidth = ", imwidth, "imheight = ", imheight)
+        array = [0x33] * 134400                                 # Setting All White
+        for y in range(imheight):
+            for x in range(imwidth):
+                if pixels[x, y] < 64:                           # Black
+                    n_pixels = y * self.width + x
+                    if n_pixels % 2 == 1:
+                        array[n_pixels//2] &= 0xF0
+                    else:
+                        array[n_pixels//2] &= 0x0F
+
+        print("%s" %(time.time() - start_time))
+
+        return array
+
+
+    def display(self, array):
+        start_time = time.time()
+        self.send_command(DATA_START_TRANSMISSION_1)
+
+        for i in range(0, 134400):
+            self.send_data(array[i])
+
+        #
+        # for i in range(0, self.width // 4 * self.height):
+        #     temp1 = image[i]
+        #     j = 0
+        #
+        #     while j < 4:
+        #         if (temp1 & 0xC0) == 0xC0:
+        #             temp2 = 0x03
+        #         else:
+        #             temp2 = 0x00
+        #
+        #         temp2 = temp2 << 4
+        #         temp1 = temp1 << 2
+        #
+        #         if(temp1 & 0xC0) == 0xC0:
+        #             temp2 |= 0x03
+        #         else:
+        #             temp2 |= 0x00
+        #
+        #         temp1 = temp1 << 2
+        #         self.send_data(temp2)
+        #
+        #         j += 2
+
+        print("%s" % (time.time() - start_time))
+        self.send_command(DISPLAY_REFRESH)
+        epdconfig.delay_ms(100)
+        self.wait_until_idle()
+
+    def Clear(self):
+        start_time = time.time()
+        self.send_command(DATA_START_TRANSMISSION_1)
+        for i in range(0, 134400):
+            self.send_data(0x33)
+        print("%s" % (time.time() - start_time))
+        self.send_command(DISPLAY_REFRESH)
+        self.wait_until_idle()
+
+
+    def sleep(self):
+        self.send_command(POWER_OFF)
+        self.wait_until_idle()
+        self.send_command(DEEP_SLEEP)
+        self.send_data(0XA5)
+### END OF FILE ###
+
---
+++ e-Paper/main.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python
+# -*- coding:utf-8 -*-
+
+import epd5in83
+import time
+import qrcode
+from PIL import Image,ImageDraw,ImageFont
+import traceback
+
+font_dir = '/usr/share/fonts/truetype/nanum'
+
+qr_code = qrcode.QRCode(version=3,
+                        error_correction=qrcode.constants.ERROR_CORRECT_H,
+                        box_size=5,
+                        border=0)
+
+try:
+    panel = epd5in83.EPD()
+    panel.init()
+    panel.Clear()
+    Image_Palette = Image.new('1', (epd5in83.EPD_WIDTH, epd5in83.EPD_HEIGHT), 255)
+
+    Image_Palette = Image.open('background.bmp')
+    Image_Palette = Image_Palette.convert('1')
+    panel.getbuffer(Image_Palette)
+
+    print("Plz Input Value")
+    value = 10
+
+    qr_code.add_data(value)
+    qr_code.make()
+
+    img_qr = qr_code.make_image()
+    img_qr.save("qr.bmp")
+
+    bmp = Image.open('qr.bmp')
+
+    bmp = Image.open('qr.bmp')
+    Image_Palette.paste(bmp, (77,147))
+    Image_Palette.save("check.bmp")
+    panel.display(panel.getbuffer(Image_Palette))
+
+    panel.sleep()
+
+
+except:
+    print('traceback.format_exc():\n%s',traceback.format_exc())
+    exit()
